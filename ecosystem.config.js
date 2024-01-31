df82e15a033310bd3e12a2ee3a71145a4decc730
Elgara5364 <steveloardi5364@gmail.com>
2024-01-31 13:01:32 +0900

    change port on ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,18 +2,8 @@ module.exports = {
   apps: [
     {
       name: "fd25-tailwind",
-      instances: "max", // Or a number of instances
       script: "node_modules/next/dist/bin/next",
-      args: "start -s build -p 80",
-      env_local: {
-        APP_ENV: "local", // APP_ENV=local
-      },
-      env_dev: {
-        APP_ENV: "dev", // APP_ENV=dev
-      },
-      env_prod: {
-        APP_ENV: "prod", // APP_ENV=prod
-      },
+      args: "start -s build ",
     },
   ],
 };
